aed65923239b5356ca391e5d117c0d27dcbc57a1
Ott-cop <otavio.onoue@outlook.com>
2024-01-04 23:49:36 -0300

    Resolving some bugs with NVS insertion data

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use log::info;
 const SSID: &str = "Apollo_2.4G";
 const PASSWORD: &str = "apollo_tplink*#";
 const STACK_SIZE: usize = 12288;
-const MAX_STR_LEN: usize = 200;
+const MAX_STR_LEN: usize = 300;
 
 
 fn main() {
@@ -67,34 +67,35 @@ fn main() {
     };
     let data = data.dump();
     
-    let value: String = match nvs_data.clone().lock().unwrap().get_str(comp_tag, &mut buffer_data) {
-        std::result::Result::Ok(v) => {
-            
-            if v.is_some() {
-                let trimmed_v = v.unwrap().trim_end_matches(char::from(0));
-                trimmed_v.to_string()
-            } else {
-                data.clone()
-            }
+    let mut value = String::new();
+    let getvalue = nvs_data.lock().unwrap().get_str(comp_tag, &mut buffer_data);
+    if getvalue.is_err() {
+        println!("[!] Problem to obtain data of NVS...");
+    } else {
+        let getvalue = getvalue.unwrap();
+        if getvalue.is_none() {
+            println!("[+] Doesnt have data!");
+            println!("[+] Setting default...");
+            value = data;
+            let _ = nvs_data.lock().unwrap().set_str(comp_tag, &value);
+        } else {
+            println!("[+] Success to obtain data!");
+            value = getvalue.unwrap().to_string();
         }
-        Err(_) => {
-            let _ = nvs_data.clone().lock().unwrap().set_str(comp_tag, data.trim_end_matches(char::from(0)));
-            data.clone()
-        }
-    }; 
+    }
 
-    std::thread::sleep(Duration::from_secs(3));
-    let valor = Arc::new(Mutex::new(json::parse(&value.trim_end_matches(char::from(0))).unwrap()));
-    let valor2 = valor.clone();
+    let json_value = Arc::new(Mutex::new(json::parse(&value.trim_end_matches(char::from(0))).unwrap()));
+    
+    let json_value2 = json_value.clone();
     let comp_1 = Arc::new(Mutex::new(PinDriver::output(peripherals.pins.gpio2).unwrap()));
     let comp_2 = Arc::new(Mutex::new(PinDriver::output(peripherals.pins.gpio13).unwrap()));
     
-    if valor.lock().unwrap()["comp_1"] == JsonValue::String("on".to_string()) {
+    if json_value.lock().unwrap()["comp_1"] == JsonValue::String("on".to_string()) {
         comp_1.lock().unwrap().set_high().unwrap();
     } else {
          comp_1.lock().unwrap().set_low().unwrap();
     }
-    if valor.lock().unwrap()["comp_2"] == JsonValue::String("on".to_string()) {
+    if json_value.lock().unwrap()["comp_2"] == JsonValue::String("on".to_string()) {
         comp_2.lock().unwrap().set_high().unwrap();
     } else {
         comp_2.lock().unwrap().set_low().unwrap();
@@ -102,9 +103,10 @@ fn main() {
 
     let nvs_storage = nvs_data.clone();
     let nvs_storage2 = nvs_data.clone();
+
     server.fn_handler("/", Method::Post, move |mut req| {
         let mut nvs_storage = nvs_storage.lock().unwrap();
-        let mut valor = valor2.lock().unwrap();
+        let mut json_value = json_value2.lock().unwrap();
         let mut buffer = [0u8; MAX_STR_LEN];
         req.read(&mut buffer).unwrap();       
         
@@ -117,30 +119,30 @@ fn main() {
             return Result::Err(HandlerError::new("Problems to parse JSON"));
         }
         let json_data = json_data.unwrap();
-        println!("Valor da data: {:?}", json_data);
+        println!("json_value da data: {:?}", json_data);
 
         if json_data["comp_1"] == JsonValue::String("on".to_string()) {
             comp_1.set_high().unwrap();
-            valor["comp_1"] = JsonValue::String("on".to_string());
+            json_value["comp_1"] = JsonValue::String("on".to_string());
         } else if json_data["comp_1"] == JsonValue::String("off".to_string()) {
             comp_1.set_low().unwrap();
-            valor["comp_1"] = JsonValue::String("off".to_string());
+            json_value["comp_1"] = JsonValue::String("off".to_string());
         }
         if json_data["comp_2"] == JsonValue::String("on".to_string()) {
             comp_2.set_high().unwrap();
-            valor["comp_2"] = JsonValue::String("on".to_string());
+            json_value["comp_2"] = JsonValue::String("on".to_string());
         } else if json_data["comp_2"] == JsonValue::String("off".to_string()) {
             comp_2.set_low().unwrap();
-            valor["comp_2"] = JsonValue::String("off".to_string());
+            json_value["comp_2"] = JsonValue::String("off".to_string());
         }
         
         unsafe { esp_idf_svc::sys::vTaskDelay(10) };
-        let _ = nvs_storage.set_str(comp_tag, valor.dump().trim_end_matches(char::from(0)));
+        let _ = nvs_storage.set_str(comp_tag, json_value.dump().trim_end_matches(char::from(0)));
         unsafe { esp_idf_svc::sys::vTaskDelay(10) };
         let mut response_buffer: [u8; MAX_STR_LEN] = [0; MAX_STR_LEN];
         
         unsafe { esp_idf_svc::sys::vTaskDelay(10) };
-        // println!("{:?}", nvs_storage.get_str(comp_tag, &mut response_buffer));
+        
         let response_data = nvs_storage.get_str(comp_tag, &mut response_buffer).unwrap().unwrap();
         unsafe { esp_idf_svc::sys::vTaskDelay(10) };
         
@@ -156,11 +158,11 @@ fn main() {
     }).unwrap()
     .fn_handler("/", Method::Get, move |mut req| {
         
-        let mut response_buffer: [u8; MAX_STR_LEN] = [0; MAX_STR_LEN];
-
-        unsafe { esp_idf_svc::sys::vTaskDelay(10) };
         
-        let response_data = nvs_storage2.lock().unwrap().get_str(comp_tag, &mut response_buffer).unwrap().unwrap();
+        let nvs_storage = nvs_storage2.lock().unwrap();
+        unsafe { esp_idf_svc::sys::vTaskDelay(10) };
+        let mut response_buffer = [0u8; MAX_STR_LEN];
+        let response_data = nvs_storage.get_str(comp_tag, &mut response_buffer).unwrap().unwrap();
         unsafe { esp_idf_svc::sys::vTaskDelay(10) };
         
         println!("Value = {:?}", response_data);
@@ -176,8 +178,8 @@ fn main() {
             }
         }
         
-
         Result::Ok(())
+
     }).unwrap();
 
    
@@ -206,10 +208,10 @@ async fn create_server<'a>(wifi: &mut AsyncWifi<EspWifi<'static>>) -> Result<Esp
 
     wifi.set_configuration(&wifi_configuration).unwrap();
 
-    wifi.start().await.unwrap();
+    let _ = wifi.start().await;
     info!("Wifi started");
 
-    wifi.connect().await.unwrap();
+    let _ = wifi.connect().await;
     info!("Wifi connected");
 
     
@@ -229,12 +231,12 @@ async fn create_server<'a>(wifi: &mut AsyncWifi<EspWifi<'static>>) -> Result<Esp
         }
     }
 
-    wifi.wait_netif_up().await.unwrap();
+    let _ = wifi.wait_netif_up().await;
     info!("Wifi netif up");
 
     let server_configuration = esp_idf_svc::http::server::Configuration {
         stack_size: STACK_SIZE,
-        session_timeout: Duration::ZERO,
+        session_timeout: Duration::MAX,
         ..Default::default()
     };
     Ok(EspHttpServer::new(&server_configuration).unwrap())
